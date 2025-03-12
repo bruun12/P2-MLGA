490e206331fb221c676c5f91eb80750fcb6c8d4d
Peter Bødstrup <HO77IY@student.aau.dk>
2025-03-12 18:35:03 +0100

    html js css
    Lidt mappe og lidt js og lidt html

---
+++ JS/Login.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    if (username === "" || password === "") {
+      alert("Please enter both username and password");
+      event.preventDefault();
+    }
+  });

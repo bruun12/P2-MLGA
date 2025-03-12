ad221d1a5a17040d257d6c2a5ed951b9c9c44d4d
Peter Bødstrup <HO77IY@student.aau.dk>
2025-03-12 19:44:22 +0100

    Html
    Fikset lidt videre

--- JS/Login.js
+++ JS/Login.js
@@ -7,3 +7,22 @@ document.querySelector("form").addEventListener("submit", function(event) {
       event.preventDefault();
     }
   });
+
+  //Lille test funktion
+  var attempt = 4;
+  function validate(){
+        if (undername == "ElskerNorskeMænd" && password == "12345678") {
+            alert("Login succesfully");
+            window.location = "Brugerprofil.html"; // Dette skal ændres når de andre sider er færdige
+            return false;
+        } else {
+            attempt--;
+            alert("Du har kun " + attempt + "forsøg tilbage")
+            if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true; 
+            document.getElementById("login").disabled = true;
+            return false;
+            } 
+        }
+    };

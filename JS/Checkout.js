e10ee42747e23c04b41529ced92f9478878fb51d
bkljucic <bkljuc24@student.aau.dk>
2025-03-13 13:11:54 +0100

    Checkoutpage opstart + lidt css og lidt js

---
+++ JS/Checkout.js
@@ -0,0 +1,8 @@
+function rabat() {
+    const rabatInput = document.getElementById("rabat").value; // Get the value of the input field
+    if (rabatInput === "timler20") {
+        alert("Du er blevet hacket din nazist");
+    } else {
+        alert("1 forsøg tilbage eller du bliver data scraped");
+    }
+}

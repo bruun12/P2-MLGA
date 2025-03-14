30cedf90525f01dc24215f7911705d74d8a0d1c9
bkljucic <bkljuc24@student.aau.dk>
2025-03-14 12:46:29 +0100

    rabatkode n

--- JS/Checkout.js
+++ JS/Checkout.js
@@ -1,5 +1,7 @@
 function rabat() {
     const rabatInput = document.getElementById("rabat").value; // Get the value of the input field
+    if (rabatInput === "NorskSnack")
+        alert("25% af din ordrer, den er 'mums'fri + fri fragt på the 'package'")
     if (rabatInput === "timler20") {
         alert("Du er blevet hacket din nazist");
     } else {

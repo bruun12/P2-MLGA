ce4c9f8e7b56f5dd4d888b6f4bb33c467516fb54
bkljucic <bkljuc24@student.aau.dk>
2025-03-14 12:49:06 +0100

    tim rabat

--- JS/Checkout.js
+++ JS/Checkout.js
@@ -3,7 +3,7 @@ function rabat() {
     if (rabatInput === "NorskSnack")
         alert("25% af din ordrer, den er 'mums'fri + fri fragt på the 'package'")
     if (rabatInput === "timler20") {
-        alert("Du er blevet hacket din nazist");
+        alert("hvaaa du syns nok det lækkert med 20% af prisen din lille gris");
     } else {
         alert("1 forsøg tilbage eller du bliver data scraped");
     }

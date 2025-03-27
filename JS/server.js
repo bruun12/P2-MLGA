8b58813237afe05746384eb54c85e311a932c735
Tim <tfni24@student.aau.dk>
2025-03-27 12:45:37 +0100

    stripe semi virker

--- JS/server.js
+++ JS/server.js
@@ -1,7 +1,5 @@
-// This test secret API key is a placeholder. Don't include personal details in requests with this key.
-// To see your test secret API key embedded in code samples, sign in to your Stripe account.
-// You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
-const stripe = require('stripe')('sk_test_4RxUm8rtZ0phcTFLbHvkTJD5');
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51R7EcUIX6mAD0y171jVAoalHC8iOsp5iTOiaDaKqNkuufYwPBmbVxRQSHQdX8JKam4PrC3h42G1VGJxe9R1xaZxX00wRN89NBf');
 const express = require('express');
 const app = express();
 app.use(express.static('Html'));
@@ -10,28 +8,19 @@ const YOUR_DOMAIN = 'http://localhost:4242';
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
-    ui_mode: 'embedded',
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1MoBy5LkdIwHu7ixZhnattbh',
+        price: 'price_1R7EfMIX6mAD0y17cDftI52v',
         quantity: 1,
       },
     ],
     mode: 'payment',
-    return_url: `${YOUR_DOMAIN}/return.html`,
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
   });
 
-  res.send({clientSecret: session.client_secret});
-});
-
-app.get('/session-status', async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-
-  res.send({
-    status: session.status,
-    customer_email: session.customer_details.email
-  });
+  res.redirect(303, session.url);
 });
 
 app.listen(4242, () => console.log('Running on port 4242'));

7f05570508a477e0e81bcb4c1b658eff083c9572
Tim <tfni24@student.aau.dk>
2025-03-27 12:21:13 +0100

    loader siden, men det virker ikke. 😢

---
+++ JS/server.js
@@ -0,0 +1,37 @@
+// This test secret API key is a placeholder. Don't include personal details in requests with this key.
+// To see your test secret API key embedded in code samples, sign in to your Stripe account.
+// You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
+const stripe = require('stripe')('sk_test_4RxUm8rtZ0phcTFLbHvkTJD5');
+const express = require('express');
+const app = express();
+app.use(express.static('Html'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1MoBy5LkdIwHu7ixZhnattbh',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    return_url: `${YOUR_DOMAIN}/return.html`,
+  });
+
+  res.send({clientSecret: session.client_secret});
+});
+
+app.get('/session-status', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+
+  res.send({
+    status: session.status,
+    customer_email: session.customer_details.email
+  });
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));

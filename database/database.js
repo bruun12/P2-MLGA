e7ab7648d24f084ef649a3cd4aa35623d36c6c28
Abtin Rezvani <151622443+abtrez@users.noreply.github.com>
2025-04-01 00:15:28 +0200

    db connection, schema WIP, server.js err log
    database.js sets up connection to database. Use test database by providing variables in .env.
    
    schema draft added, might throw error
    
    error logging in server.js

---
+++ database/database.js
@@ -0,0 +1,18 @@
+/* General:
+ * Purpose of file: Create connection to database
+      Set environment variables in your own .env file with the credentials of your personal MYSQL TEST database
+      i.e. MYSQL_HOST='localhost' 
+ * Schema and queries in other files
+*/
+
+import mysql from 'mysql';
+
+/* Connection pooling to improve performance, avoid overloading MYSQL server with too many connections
+  creates a pool of reusable connections to the database - instead of creating a new connection for each query*/
+const dbPool = mysql.createPool({
+    //Environment variables for: sensitive info & easy configuration/change of database
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();    //promise() allows use promise API version of mysql
--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const port = process.env.NODE0 || 3350;
 //Initialize express
 const app = express();
 
-// Use static files 
+//Use static files 
 app.use(express.static('public'));
+
+//Reveal error if any
+app.use((error, request, response, next) => {
+    console.error(error.stack);
+    response.status(500).send('Something broke! Check logs');
+});
 
 // Route test
 app.get("/", (request, response) => {
     response.send("Hello world");
-})
+});
 
 //Start the server & listen on a port
 app.listen(port, () => {

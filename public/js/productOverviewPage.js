fb5401e2e0eb11eeffe056926759ab6b908e848b
8ruun <mbja24@student.aau.dk>
2025-04-02 14:09:25 +0200

    productPage updated

--- public/js/productOverviewPage.js
+++ public/js/productOverviewPage.js
@@ -66,7 +66,7 @@ function productDisplay(name, price, img){
 }
 
 
-fetch("../database/products.json")
+fetch("../../database/products.json")
 //Her omskriver vi det fra json til et array i js. Arrayet hedder "data" i næste function
 .then(response => {return response.json()})
 .then(data=>{

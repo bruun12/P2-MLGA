845bd1fd6d1039b555741db737f0eef3622c519e
8ruun <mbja24@student.aau.dk>
2025-04-02 12:06:42 +0200

    hej

---
+++ public/js/guide.js
@@ -0,0 +1,8 @@
+"use strict"
+dawaAutocomplete.dawaAutocomplete( document.getElementById("adresse"), {
+  select: function(selected) {
+    document.getElementById("valgtadresse").innerHTML= selected.tekst;
+  }
+});
+
+console.log("hej");

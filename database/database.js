5c27bc435baacd875a5fd4111928c12f795c16fc
Markus <vt00cl@student.aau.dk@p2datsw-f25-01.srv.aau.dk>
2025-04-04 22:21:03 +0200

    jeg har leget lidt med db og det virker faktisk lidt

--- database/database.js
+++ database/database.js
@@ -5,14 +5,19 @@
  * Schema and queries in other files
 */
 
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
 /* Connection pooling to improve performance, avoid overloading MYSQL server with too many connections
   creates a pool of reusable connections to the database - instead of creating a new connection for each query*/
 const dbPool = mysql.createPool({
+
     //Environment variables for: sensitive info & easy configuration/change of database
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: "localhost", //process.env.MYSQL_HOST,
+    user: "cs-25-sw-2-05@student.aau.dk", //process.env.MYSQL_USER,
+    password: "FKmPu3CC@2CbPPPj", //process.env.MYSQL_PASSWORD,
+    database: "cs_25_sw_2_05" //process.env.MYSQL_DATABASE
 }).promise();    //promise() allows use promise API version of mysql
+
+const result = await dbPool.query("SELECT street FROM address")
+
+console.log(result);

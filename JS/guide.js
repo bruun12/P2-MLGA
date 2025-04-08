5b79f342651e4c6722a613f9b005bd16c904ff0f
bkljucic <bkljuc24@student.aau.dk>
2025-04-08 08:36:15 +0200

    autoudfyld implementeret

--- JS/guide.js
+++ JS/guide.js
@@ -1,7 +1,7 @@
 "use strict"
 dawaAutocomplete.dawaAutocomplete( document.getElementById("adresse"), {
   select: function(selected) {
-    document.getElementById("valgtadresse").innerHTML= selected.tekst;
+    document.getElementById("valgtadresse").innerText= selected.tekst;
   }
 });
 

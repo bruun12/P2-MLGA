aa531e3cbe98700bc556559df04251e2ab027f4f
TimFangel <tfni24@student.aau.dk>
2025-05-09 09:57:48 +0200

    strip

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -49,3 +49,27 @@ function getCookie(name){
     })
     return result;
 } 
+
+let checkOutBtn = document.querySelector("#maddog");
+checkOutBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    fetch('/create-checkout-session', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            items: [
+                { id: 1, quantity: 3 },
+                { id: 2, quantity: 1 }
+            ]
+        })
+    }).then(res => {
+        if (res.ok) return res.json();
+        return res.json().then(json => Promise.reject(json));
+    }).then(({ url }) => {
+        window.location = url
+    }).catch(error => {
+        console.error(error.error);
+    });
+});

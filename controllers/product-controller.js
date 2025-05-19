8899c7075b9c9ea860f6aeeddd7497387d170fef
8ruun <mbja24@student.aau.dk>
2025-05-19 08:51:14 +0200

    :)

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -12,7 +12,6 @@ import {getAllProducts, getAllCategories, filteredProducts, searchedProducts, pr
 
 /* Uses getAllProducts from product-model to return a list of all products in json format */
 export const getProducts = async (req, res) => {
-  console.log("jeg bliver brugt");
     try {
       const products = await getAllProducts();
       res.json(products); // Return JSON response
